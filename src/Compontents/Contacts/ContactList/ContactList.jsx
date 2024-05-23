c29fc653af71b1c83ae6e8991d91f9e22364dcf2
Rohit Aute <rohit.aute450@gmail.com>
2024-05-23 23:00:24 +0530

    Search Function Issue Solve

--- src/Compontents/Contacts/ContactList/ContactList.jsx
+++ src/Compontents/Contacts/ContactList/ContactList.jsx
@@ -5,129 +5,148 @@ import Spinner from '../../Spinner/Spinner'
 import toast from 'react-hot-toast'
 
 const ContactList = () => {
-  let navigate = useNavigate()
-  let {contactID}=useParams()
-  let [state,setState]=useState({
-    loading:false,
-    contact:[],
-    errorMessage:""
-  })
-
-  useEffect(()=>{
-    let promise = new Promise((resolve, reject) => {
-      setState({...state, loading: true});
-      let prom = ContactService.getAllContact();
-      prom.then(res1 => {
-        resolve(res1);
-      }).catch(err => {
-        reject(err);
-      });
-    });
-    
-    promise.then((res1) => {
-      setState({...state, loading: false, contact: res1.data});
-    }).catch((rej1) => {
-      setState({...state, loading: false, errorMessage: toast.error("Data not Found")});
-    });
-  },[])
-  let {loading,contact,errorMessage}=state;
+  let navigate = useNavigate();
+  let { contactID } = useParams();
   
+  let [query, setQuery] = useState({
+    text: "",
+  });
+  
+  let [state, setState] = useState({
+    loading: false,
+    contacts: [], // Corrected state property name
+    filteredContacts: [], // Corrected state property name
+    errorMessage: "",
+  });
+
+  useEffect(() => {
+    setState(prevState => ({ ...prevState, loading: true }));
+
+    ContactService.getAllContact()
+      .then(response => {
+        setState(prevState => ({
+          ...prevState,
+          loading: false,
+          contacts: response.data,
+          filteredContacts: response.data,
+        }));
+      })
+      .catch(error => {
+        setState(prevState => ({
+          ...prevState,
+          loading: false,
+          errorMessage: "Data not found",
+        }));
+        toast.error("Data not found");
+      });
+  }, []);
+
   const deleteHandle = (contactID) => {
     ContactService.deleteContact(contactID)
-      .then((response) => {
+      .then(response => {
         if (response.status === 200) {
-          setState({
-            ...state,
-            contacts: contact.filter(contact => contact.id !== contactID),
-          });
-          
-          toast.success("Contact Deleted Successfully!!")
-          setTimeout(()=>{
-            window.location.reload();
-        },1000)
+          setState(prevState => ({
+            ...prevState,
+            contacts: prevState.contacts.filter(contact => contact.id !== contactID),
+            filteredContacts: prevState.filteredContacts.filter(contact => contact.id !== contactID),
+          }));
+          toast.success("Contact Deleted Successfully!!");
         } else {
           toast.error("Failed to delete the contact");
         }
       })
-      .catch((error) => {
+      .catch(error => {
         toast.error("Error deleting contact");
       });
   };
+
+  const SearchContact = (event) => {
+    setQuery({ ...query, text: event.target.value });
+    let filteredContacts = state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(event.target.value.toLowerCase())
+    );
+    setState(prevState => ({ ...prevState, filteredContacts }));
+  };
+
+  let { loading, contacts, filteredContacts, errorMessage } = state;
+
   return (
     <>
-    {/* <pre>{JSON.stringify(contact)}</pre> */}
       <section className='contact-search p-3'>
         <div className="container">
           <div className="grid">
             <div className="row">
-              <p className='h3'>Contact Manger <Link to={'/contacts/add'} className='btn btn-primary'><i className='fa fa-plus-circle me-2'></i>New</Link></p>
+              <p className='h3'>
+                Contact Manager <Link to={'/contacts/add'} className='btn btn-primary'>
+                  <i className='fa fa-plus-circle me-2'></i>New
+                </Link>
+              </p>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus perferendis et saepe! Deleniti velit a ullam itaque nostrum omnis veritatis praesentium iusto? Accusantium sed iste quia incidunt, reprehenderit assumenda est?</p>
             </div>
             <div className="row">
               <div className="col-md-6">
                 <div className="row">
                   <div className="col-md-8">
-                    <input type="search" name="" placeholder='Search Name' id="" className='form-control' />
+                    <input 
+                      type="search" 
+                      name='text' 
+                      value={query.text} 
+                      onChange={SearchContact} 
+                      placeholder='Search Name' 
+                      className='form-control' 
+                    />
                   </div>
                   <div className="col-md-4">
-                    <input type="submit" value='Search' className='btn btn-outline-dark' />
+                    <input type="submit" name='submit' className='btn btn-outline-dark' />
                   </div>
-
                 </div>
               </div>
             </div>
 
-
-            <section className='contact-card '>
+            <section className='contact-card'>
               <div className="container my-5">
                 <div className="row">
-                  {
-                    loading?<Spinner/>:<>
-                    {
-                      contact.map((cont)=>{
-                         return(
-                          <div className="col-md-6  p-3 ">
-                    <div className="row ">
-                      <div className="card">
-                        <div className="card-body">
-                          <div className="row d-flex align-items-center">
-                            <div className="col-md-4">
-                              <img src={cont.photo} alt="" className='image-fulid contact-image' />
-                            </div>
-                            <div className="col-md-7">
-                              <ul className='list-group'>
-                                <li className="list-group-item list-group-item-action">Name:<span className='fw-blod ms-1'>{cont.name}</span></li>
-                                <li className="list-group-item list-group-item-action">Email:<span className='fw-blod ms-1'>{cont.email}</span></li>
-                                <li className="list-group-item list-group-item-action">Contact:<span className='fw-blod ms-1'>{cont.contact}</span></li>
-
-                              </ul>
-                            </div>
-                            <div className="col-md-1 d-flex flex-column p-1">
-                              <Link to={`/contacts/view/${cont.id}`} className='btn btn-warning my-1 p-1'><i className='fa fa-eye' /></Link>
-                              <Link to={`/contacts/edit/${cont.id}`} className='btn btn-primary my-1 p-1'><i className='fa fa-pen' /></Link>
-                              <button className='btn btn-danger my-1' onClick={() => deleteHandle(cont.id)}><i className='fa fa-trash' /></button>
+                  {loading ? <Spinner /> : (
+                    filteredContacts.length > 0 ? (
+                      filteredContacts.map(cont => (
+                        <div className="col-md-6 p-3" key={cont.id}>
+                          <div className="card">
+                            <div className="card-body">
+                              <div className="row d-flex align-items-center">
+                                <div className="col-md-4">
+                                  <img src={cont.photo} alt="" className='image-fluid contact-image' />
+                                </div>
+                                <div className="col-md-7">
+                                  <ul className='list-group'>
+                                    <li className="list-group-item list-group-item-action">Name:<span className='fw-bold ms-1'>{cont.name}</span></li>
+                                    <li className="list-group-item list-group-item-action">Email:<span className='fw-bold ms-1'>{cont.email}</span></li>
+                                    <li className="list-group-item list-group-item-action">Contact:<span className='fw-bold ms-1'>{cont.contact}</span></li>
+                                  </ul>
+                                </div>
+                                <div className="col-md-1 d-flex flex-column p-1">
+                                  <Link to={`/contacts/view/${cont.id}`} className='btn btn-warning my-1 p-1'><i className='fa fa-eye' /></Link>
+                                  <Link to={`/contacts/edit/${cont.id}`} className='btn btn-primary my-1 p-1'><i className='fa fa-pen' /></Link>
+                                  <button className='btn btn-danger my-1' onClick={() => deleteHandle(cont.id)}><i className='fa fa-trash' /></button>
+                                </div>
+                              </div>
                             </div>
                           </div>
                         </div>
+                      ))
+                    ) : (
+                      <div className="col-md-12">
+                        <p className="text-center">No contacts found</p>
                       </div>
-                    </div>
-                  </div>
-                         )
-                      })
-                    }
-                    </>
-                  }
-      
+                    )
+                  )}
                 </div>
               </div>
-
             </section>
           </div>
         </div>
-
       </section>
     </>
-  )
-}
+  );
+};
 
-export default ContactList
+export default ContactList;
